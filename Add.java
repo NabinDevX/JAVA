305f70cb78722fb44c7c200618846f2610dbf841
nabinguy <nabinbera999@gmail.com>
2025-07-21 16:31:05 +0000

    2

---
+++ Add.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.Scanner;
+class Add{
+    public static void main(String args[]){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter 1st number: ");
+        int n1 = scanner.nextInt();
+        System.out.print("Enter 2nd number: ");
+        int n2 = scanner.nextInt();
+        System.out.println("The sum is "+(n1+n2));
+    }
+}
